cde352eb2ff698795a40c0f17d8b6cdf5c1fca11
KyleChutjian <kyle.chutjian@gmail.com>
2021-02-23 17:10:41 -0500

    Created the base of the program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
 
-    
+    public static void main(String[] args) {
+        Scanner IO = new Scanner(System.in);
+        System.out.println("This program judges your opinions, dont have bad ones");
+
+
+    }
 
 }
